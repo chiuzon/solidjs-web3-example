64b8d8784bd057291aef6f53f7041c72de0ce5b2
as <89428507+chiuzon@users.noreply.github.com>
2021-12-11 18:49:37 +0200

    provider function

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,46 @@
-import type { Component } from "solid-js";
+import { Component, createEffect, createMemo } from "solid-js";
 
 import {InjectedConnector} from '@web3-react/injected-connector'
 import {solidWeb3, Web3Provider} from '@chiuzon/solidweb3'
 
 import {ethers} from 'ethers'
 
-const injectedConnector = new InjectedConnector({supportedChainIds: [1]})
+const injectedConnector = new InjectedConnector({supportedChainIds: [41]})
+
+const rpcProvider = new ethers.providers.JsonRpcProvider("https://testnet.telos.net/evm")
+
+const useWeb3 = () => {
+  const { library } = solidWeb3()
+  
+  return createMemo(() => {
+      return library() || rpcProvider
+  })
+}
+
+const ERC20Abi = [
+  "function balanceOf(address) external view returns(uint256)"
+]
+
+const erc20Contract = (address: string) => {
+  const lib = useWeb3()
+
+  if(!ethers.utils.getAddress(address)){
+    return undefined
+  }
+
+  return createMemo(() => {
+    return new ethers.Contract(address, ERC20Abi, lib())
+  })
+}
 
 const InnerApp: Component = () => {
-  const {activate, store} = solidWeb3()
+  const {activate, library, account} = solidWeb3()
+  
+  const web3Lib = useWeb3()
+
+  createEffect(() => {
+    console.log(web3Lib())
+  })
 
   async function connectButton() {
     await activate(injectedConnector, (error) => {
@@ -18,7 +50,7 @@ const InnerApp: Component = () => {
 
   return (
     <>
-        {store.account}
+        {account()}
         <h1>Example</h1>
         <button onClick={connectButton}>Connect</button>
     </>
